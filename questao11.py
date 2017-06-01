2280ab6229e68cb79fc5102d7b7bd9bec36fc1bf
Emerson17TJ <remerson476@gmail.com>
2017-06-01 10:26:58 -0300

    questao11

---
+++ questao11.py
@@ -0,0 +1,4 @@
+n1=int(input ("Digite o primeiro numero:"))
+n2=int(input ("Digite o segundo numero:"))
+n3=int(input ("Digite o terceiro numero:"))
+print("A soma do primeiro e segundo numero multiplicado pela soma do segundo pelo terceiro é igual a",(n1+n2)*(n2+n3))
